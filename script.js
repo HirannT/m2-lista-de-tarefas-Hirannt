e94ece82541e350c4b5f47e2d27ba30fc9fc6d21
Hiran <hirantheodozio@gmail.com>
2025-01-30 21:18:02 -0300

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tasks = [
+var tasks = [
   {title: "Comprar comida para o gato", type: "Urgente"},
   {title: "Consertar Computador", type: "Importante"},
   {title: "Beber água", type: "Normal"},
@@ -8,5 +8,74 @@ const tasks = [
   {title: "Ler pelo menos um capítulo de um livro", type: "Normal"},
   {title: "Limpar a despensa", type: "Importante"},
   {title: "Pagar a conta de energia", type: "Urgente"},
-  {title: "Assistir a um documentário interessante", type: "Normal"},
+  {title: "Assistir a um documentário interessante", type: "Normal"}
 ];
+
+renderElements(tasks);
+addTaskOnList();
+
+function renderElements(tasks) {
+    
+  let html = ``;
+  
+  for (var i = 0; i < tasks.length; i++) {
+    
+    let task = tasks[i];
+    html += createTaskItem(task, i);
+  }
+  
+  document.querySelector('.tasks__list').innerHTML = html;
+}
+
+function createTaskItem(task, i) {
+
+  let classTypeTask = ``; 
+
+  if(task.type == 'Urgente') {
+    classTypeTask = `span-urgent`;
+  }
+  else if(task.type == 'Importante') {
+    classTypeTask = `span-important`;
+  }
+  else if(task.type == 'Normal') {
+    classTypeTask = `span-normal`; 
+  }
+
+  let html = `
+    <li class="task__item">
+      <div class="task-info__container">
+        <span class="task-type ${classTypeTask}"></span>
+        <p>${task.title}</p>
+      </div>
+      <button class="task__button--remove-task" onclick="removeTaskItem(${i})"></button>
+    </li>  
+  `; 
+
+  return html;
+}
+
+function addTaskOnList() {
+  
+  let form = document.querySelector(".form__button--add-task");
+
+  form.addEventListener('click',  function(event) {
+
+    event.preventDefault();
+  
+    let task = {
+      title: document.querySelector('.form__input--text').value,
+      type: document.querySelector('.form__input--priority').value
+    };
+
+    tasks.push(task);
+
+    renderElements(tasks);
+  });
+}
+
+function removeTaskItem(i) {
+
+  tasks.splice(i, 1);
+  renderElements(tasks);
+}
+
